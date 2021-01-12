99fae56d1a9e386ef3ce27579c7aef6789ea8b7c
Koubae <58447627+Koubae@users.noreply.github.com>
2021-01-12 21:06:47 +0100

    Ready Form step 12 Jan 2021

---
+++ Front-End/Ready-Design/Forms/Step_Form/main.js
@@ -0,0 +1,30 @@
+var $firstButton = $(".first"),
+  $secondButton = $(".second"),
+  $input = $("input"),
+  $name = $(".name"),
+  $more = $(".more"),
+  $yourname = $(".yourname"),
+  $reset = $(".reset"),
+  $ctr = $(".container");
+
+$firstButton.on("click", function(e){
+  $(this).text("Saving...").delay(900).queue(function(){
+    $ctr.addClass("center slider-two-active").removeClass("full slider-one-active");
+  });
+  e.preventDefault();
+});
+
+$secondButton.on("click", function(e){
+  $(this).text("Saving...").delay(900).queue(function(){
+    $ctr.addClass("full slider-three-active").removeClass("center slider-two-active slider-one-active");
+    $name = $name.val();
+    if($name == "") {
+      $yourname.html("Anonymous!");
+    }
+    else { $yourname.html($name+"!"); }
+  });
+  e.preventDefault();
+});
+
+// copy
+balapaCop("Step by Step Form", "#999");
