7ffbe042129d08b3df2503a6f2de019cd2c50119
Koubae <58447627+Koubae@users.noreply.github.com>
2021-06-16 06:32:43 +0200

    Postgres Alphanumeric Regex query 16th-June-2021

--- Back-End/Databases/PostgreSQL/Select/select.sql
+++ Back-End/Databases/PostgreSQL/Select/select.sql
@@ -15,3 +15,15 @@ SELECT
 FROM
     customer;
     
+
+
+
+-- Select All There there aren't Numbers
+-- Here, the ^ character outside of square brackets means the beginning of the field, the $ character means the end of the field, and we require that all characters in between are non-digits. + indicates that there should be at least one such characters. If we would also allow empty strings, then the regex would look like ^[^0-9]*$.
+
+SELECT COUNT(*) FROM my_table WHERE table_record ~ '^[^0-9]+$';
+
+
+-- If you want the records which would include digits and lower-case letter, then I would expect a regex like:
+
+DELETE FROM myrecords WHERE record ~ '[0-9a-z]';
