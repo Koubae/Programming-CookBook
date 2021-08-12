71328cf16577d99febfada3fc71fff6d5e816200
Koubae <58447627+Koubae@users.noreply.github.com>
2021-08-12 22:44:08 +0200

    GITHUB.bash | 12th-August-2021

---
+++ Front-End/JavaScript/Frameworks-Libraries/React/Utils/commands.bash
@@ -0,0 +1,17 @@
+# Here's official installation process: [Create a New React App](https://reactjs.org/docs/create-a-new-react-app.html), 
+# and officially recommended [tutorials](https://reactjs.org/community/courses.html)
+
+# install react cli
+npm install -g create-react-app
+
+# create app
+create-react-app my-react-app-name
+
+# go to project folder
+cd my-react-app-name
+
+# install dependencies
+npm install
+
+# start live server
+npm start
