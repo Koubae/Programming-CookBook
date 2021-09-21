c7bad714e418c936571e1f95f4431fbd79d1a68f
Koubae <58447627+Koubae@users.noreply.github.com>
2021-09-21 19:15:04 +0200

    Added Intervals

---
+++ Back-End/Python/interval_frequency_datetime.py
@@ -0,0 +1,11 @@
+"""Requires dateutil"""
+from dateutil.relativedelta import relativedelta
+
+intervals = {
+    'work_days': lambda interval: relativedelta(days=interval),
+    'days': lambda interval: relativedelta(days=interval),
+    'hours': lambda interval: relativedelta(hours=interval),
+    'weeks': lambda interval: relativedelta(days=7 * interval),
+    'months': lambda interval: relativedelta(months=interval),
+    'minutes': lambda interval: relativedelta(minutes=interval),
+}
