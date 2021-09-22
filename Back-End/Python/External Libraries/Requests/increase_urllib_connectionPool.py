efde1b7aa23345aa228c7a81d819aab05fb14147
Koubae <58447627+Koubae@users.noreply.github.com>
2021-09-22 16:20:10 +0200

    Python Requests | connection pool :rocket: | 22-09-21

--- Back-End/Python/External Libraries/Requests/increase_urllib_connectionPool.py
+++ Back-End/Python/External Libraries/Requests/increase_urllib_connectionPool.py
@@ -4,3 +4,6 @@ session = requests.Session()
 adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
 session.mount('http://', adapter)
 response = session.get("/mypage")
+
+# Requests usess urllib3 PoolManager ( & HTTPConnectionPool ?) --- > https://urllib3.readthedocs.io/en/latest/advanced-usage.html#advanced-usage
+# Requests adapter docs --> https://docs.python-requests.org/en/latest/api/#requests.adapters.HTTPAdapter
