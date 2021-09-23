f224245d64828928f2b0a42e8700e804a44cebf6
Koubae <58447627+Koubae@users.noreply.github.com>
2021-09-23 07:16:52 +0200

    Python | Encode Multipart-data/form | 23-11-21

--- Back-End/Python/build_multipart_form_data.py
+++ Back-End/Python/build_multipart_form_data.py
@@ -40,6 +40,77 @@ print(f'content_type -> \n{content_type}')
 # multipart/form-data; boundary=e629d5811a7926f866dd67830932ed66
 
 
+# Other encoding
+# Source --> https://www.py4u.net/discuss/255896
+
+import httplib, mimetypes, android, sys, urllib2, urllib, simplejson
+
+def post_multipart(host, selector, fields, files):
+    """
+    Post fields and files to an http host as multipart/form-data.
+    fields is a sequence of (name, value) elements for regular form fields.
+    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    Return the server's response page.
+    """
+    content_type, body = encode_multipart_formdata(fields, files)
+    h = httplib.HTTP(host)
+    h.putrequest('POST', selector)
+    h.putheader('content-type', content_type)
+    h.putheader('content-length', str(len(body)))
+    h.endheaders()
+    h.send(body)
+    errcode, errmsg, headers = h.getreply()
+    return h.file.read()
+
+def encode_multipart_formdata(fields, files):
+    """
+    fields is a sequence of (name, value) elements for regular form fields.
+    files is a sequence of (name, filename, value) elements for data to be uploaded as files
+    Return (content_type, body) ready for httplib.HTTP instance
+    """
+    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+    CRLF = '\r\n'
+    L = []
+    for (key, value) in fields:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"' % key)
+        L.append('')
+        L.append(value)
+    for (key, filename, value) in files:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+        L.append('Content-Type: %s' % get_content_type(filename))
+        L.append('')
+        L.append(value)
+    L.append('--' + BOUNDARY + '--')
+    L.append('')
+    body = CRLF.join(L)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
+
+def get_content_type(filename):
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+
+host = 'www.google.co.in'
+selector = '/searchbyimage/upload'
+fields = [('user-agent','Mozilla/5.0 (Windows NT 5.1; rv:6.0.2) Gecko/20100101 Firefox/6.0.2'),('connection','keep-alive'),('referer','')]
+
+with open('jpeg.jpg', 'rb') as jpeg:
+    files = [('encoded_image', 'jpeg.jpg', jpeg.read())]
+response = post_multipart(host, selector, fields, files) #added: response = 
+responseLen=(len(response)-1)
+x=22
+if response[(x-21):(x+1)]!='EF=\"http://www.google':
+    x+=1
+x+=145
+link=''
+while response[(x+1):(x+7)]!='amp;us':  #>here<
+    link=link+response[x]
+    x+=1
+print(link)
+
+
 # ===================================================== EMAILS
 
 from email.mime import multipart
