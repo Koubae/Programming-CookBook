36b01ef514c58b373a2381e83bc9171e7d206531
Koubae <58447627+Koubae@users.noreply.github.com>
2021-09-30 10:59:07 +0200

    Postgres  commands 30-11-21

--- Back-End/Databases/PostgreSQL/string_manipulation.sql
+++ Back-End/Databases/PostgreSQL/string_manipulation.sql
@@ -4,3 +4,20 @@
 select id, SPLIT_PART(SPLIT_PART(name, '/', 1), '(', 1) from res_lang;
 select id, TRIM(BOTH ' ' FROM SPLIT_PART(SPLIT_PART(name, '/', 1), '(', 1)) from res_lang;
 select id, TRIM(SPLIT_PART(SPLIT_PART(name, '/', 1), '(', 1)) AS name from res_lang;
+select id, LOWER(TRIM(SPLIT_PART(SPLIT_PART(name, '/', 1), '(', 1))) AS name from res_lang;
+
+SELECT 
+    id, 
+    LOWER(
+        TRIM(
+            SPLIT_PART(
+                SPLIT_PART(
+                    name, -- like Chinese (Simplified) / 简体中文
+                    '/', 1
+                )
+                '(', 1
+            )
+        )
+    ) AS name
+FROM 
+    res_lang;
