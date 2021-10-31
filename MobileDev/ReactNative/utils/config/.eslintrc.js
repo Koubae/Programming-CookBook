51d55343b60907109bcfb5409dc7eb71363d7f5f
Koubae <58447627+Koubae@users.noreply.github.com>
2021-10-31 12:54:47 +0100

    markdown

--- MobileDev/ReactNative/utils/config/.eslintrc.js
+++ MobileDev/ReactNative/utils/config/.eslintrc.js
@@ -6,39 +6,57 @@ npm install --save-dev eslint-plugin-react
 
 
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest": true
-  },
-
-  "rules": {
-    "arrow-parens": ["error", "as-needed", {
-      "requireForBlockBody": true
-    }],
-    "react/jsx-props-no-spreading": "off",
-    "react/jsx-sort-props": ["error", {
-      "reservedFirst": ["key"]
-    }],
-    "react/require-default-props": "off",
-    "react/sort-prop-types": "error",
-    "react/state-in-constructor": ["error", "never"],
-    "semi-spacing": "warn"
-  },
-  "overrides": [
-    {
-      "files": [
-        "sample/**",
-        "test/**"
-      ],
-      "rules": {
-        "import/no-unresolved": "off"
-      }
-    }
-  ]
+    'env': {
+        'browser': true,
+        'es6': true,
+        'jest': true
+    },
+    'parser': 'babel-eslint',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'ecmaVersion': 8,
+            'experimentalObjectRestSpread': true,
+            'jsx': true,
+        },
+        'sourceType': 'module',
+    },
+    'plugins': ['react', 'react-native'],
+    'rules': {
+        'arrow-parens': ['error', 'as-needed', {
+            'requireForBlockBody': true
+        }],
+        'react/jsx-props-no-spreading': 'off',
+        'react/jsx-sort-props': ['error', {
+            'reservedFirst': ['key']
+        }],
+        'react/require-default-props': 'off',
+        'react/sort-prop-types': 'error',
+        'react/state-in-constructor': ['error', 'never'],
+        'semi-spacing': 'warn',
+        'indent': ['warn', 4, { 'SwitchCase': 1 }],
+        'semi': ['error', 'always'],
+        'linebreak-style': ['error', 'unix'],
+        'no-undef': ['error'],
+        'no-console': ['off'],
+        'no-unused-vars': ['warn'],
+        'react/prop-types': ['warn'],
+        'react-native/no-unused-styles': ['warn'],
+        'react-native/split-platform-components': ['warn'],
+        'react-native/no-inline-styles': ['warn'],
+        'react-native/no-color-literals': ['off'],
+    },
+    'overrides': [
+        {
+            'files': [
+                'sample/**',
+                'test/**'
+            ],
+            'rules': {
+                'import/no-unresolved': 'off'
+            }
+        }
+    ]
 };
-
 // module.exports = {
 //   root: true,
 //   extends: '@react-native-community',
