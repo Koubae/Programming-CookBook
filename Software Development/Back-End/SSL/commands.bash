c9741c9ed188c5b12b28383a0ad8bdf6286537cd
Koubae <58447627+Koubae@users.noreply.github.com>
2022-05-22 19:22:54 +0200

    Added ssl commands

--- Software Development/Back-End/SSL/commands.bash
+++ Software Development/Back-End/SSL/commands.bash
@@ -1,3 +1,9 @@
 #  generate a self-signed SSL certificate using OpenSSL?
 # Credit: https://stackoverflow.com/a/10176685/13903942
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
+
+
+# credit https://stackoverflow.com/a/41366949/13903942
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout example.key -out example.crt -subj "/CN=example.com" \
+  -addext "subjectAltName=DNS:example.com,DNS:www.example.net,IP:10.0.0.1"
