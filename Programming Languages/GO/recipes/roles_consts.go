e50271eab3c5b73f8056c4f757f02a1ba1d049d5
Koubae <58447627+Koubae@users.noreply.github.com>
2022-05-26 21:58:39 +0200

    Go: roles consts

---
+++ Programming Languages/GO/recipes/roles_consts.go
@@ -0,0 +1,25 @@
+// Role Default value with const iota
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	isAdmin = 1 << iota
+	isHeadQuarters
+	canSeeFinancials
+
+	canSeeAfrica
+	canSeeAsia
+	canSeeEurope
+	canSeeNorthAmerica
+	canSeeSouthAmerica
+)
+
+func main() {
+	var roles byte = isAdmin | canSeeFinancials | canSeeEurope
+
+	fmt.Printf("%b\n", roles)
+	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
+	fmt.Printf("Is HQ? %v\n", isHeadQuarters&roles == isHeadQuarters)
