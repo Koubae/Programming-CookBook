8cd1d1c3bbcae4c1108f6ed0c3156a4c7999b782
Koubae <58447627+Koubae@users.noreply.github.com>
2022-05-27 18:38:27 +0200

    GO: print os

---
+++ Programming Languages/GO/recipes/print_machine_os.go
@@ -0,0 +1,25 @@
+/*
+Small programm that prints the current operating system
+https://go.dev/tour/flowcontrol/9
+*/
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
+}
