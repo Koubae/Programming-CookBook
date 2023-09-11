48c68b0d900294914cd934b72d303dab911d7c8f
Koubae <58447627+Koubae@users.noreply.github.com>
2023-09-11 09:53:25 +0200

    Java -Threading

---
+++ Programming Languages/Java/Core Principles & Structure/3_advanced/MainThreading.java
@@ -0,0 +1,26 @@
+public class MainThreading {
+    public static void main(String[] args) {
+        System.out.printf("- [%s] Threading Example\n", Thread.currentThread().getName());
+
+        Thread thread = new Thread() {
+            public void run() {
+                String threadName = Thread.currentThread().getName();
+                try {
+                    System.out.printf("- [%s] start job\n", threadName);
+                    for (int i = 0; i < 10; i ++) {
+                        System.out.printf("- [%s] working on %d\n", threadName, i);
+                        Thread.sleep(450);
+                    }
+
+                } catch (InterruptedException error) {
+                    System.out.printf("- [%s] Error in thread, error %s\n", Thread.currentThread().getName(), error);
+                }
+            }
+        };
+
+        thread.start();
+
+
+    }
+    
+}
