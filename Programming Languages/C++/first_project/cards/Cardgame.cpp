0a5fbd297b57fa1b1ab0c28622e66034b7313656
Koubae <58447627+Koubae@users.noreply.github.com>
2024-09-29 12:45:27 +0200

    c++

--- Programming Languages/C++/first_project/cards/Cardgame.cpp
+++ Programming Languages/C++/first_project/cards/Cardgame.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int CardGame::totalPartecipants = 0;
 
 // Constructor
-CardGame::CardGame(int players) : players(players)
+CardGame::CardGame(int players) : players(players)  // initialization list. https://stackoverflow.com/questions/2785612/c-what-does-the-colon-after-a-constructor-mean
 {
     totalPartecipants += players; 
     cout << players << " players have started a new game.  There are now "
