062dec5451a491c400cffbcca34ddd3a52da9190
Federico Bau <58447627+Koubae@users.noreply.github.com>
2025-06-29 06:50:08 +0200

    vim commands

--- IDE - TextEditors/Vim_Family/commands.bash
+++ IDE - TextEditors/Vim_Family/commands.bash
@@ -1,4 +1,8 @@
-
+# ------------------
+# General
+# ------------------
+# Reload without re-starting
+:source ~/.vimrc
 
 # ------------------
 # Color Schemes
@@ -6,5 +10,19 @@
 
 # Shows all available colorschemes
 :colorscheme <Ctrl+d>
+# Press <Tab> repeatedly after typing :colorscheme — Vim will cycle through all installed colorschemes.
+:colorscheme <Tab>
+# Show all colors
+:echo globpath(&rtp, "colors/*.vim", 0, 1)
+# Cycle + preview
+for f in map(split(globpath(&rtp, "colors/*.vim"), "\n"), 'fnamemodify(v:val, ":t:r")') | exe "colorscheme " . f | echo f | sleep 500m | endfor
+for f in map(split(globpath(&rtp, "colors/*.vim"), "\n"), 'fnamemodify(v:val, ":t:r")') | exe "colorscheme " . f | echo f | sleep 2000m | endfor
+# better!
+:for f in map(split(globpath(&rtp, "colors/*.vim"), "\n"), 'fnamemodify(v:val, ":t:r")') | execute 'colorscheme ' . f | redraw | echo 'Now using: ' . f | sleep 2000m | endfor
+# cycle specific themes infinitelly Ctrl-C
+:while 1 | for f in ['habamax', 'evening', 'lunaperche', 'slate', 'sorbet'] | execute 'colorscheme ' . f | redraw | echo 'Now using: ' . f | sleep 2000m | endfor | endwhile
+
+
+
 :colorscheme habamax
 :colorscheme slate
